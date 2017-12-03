8bac55f6b6e60e98d997f587a9721cb476df0e88
PavelSPN <33983132+PavelSPN@users.noreply.github.com>
2017-12-03 22:45:21 +0300

    Add files via upload

---
+++ index.php
@@ -0,0 +1,10 @@
+<?php
+
+/**
+ * @file
+ * Contains php functions for start main.php page.
+ */
+
+print "<html><head> 
+  <meta HTTP-EQUIV='Refresh' CONTENT='0; URL=templates/main.php?page=1'> 
+  </head></html>";
