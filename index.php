a83a8bd741443ea185f77d1f7490081faadf686b
e.bogatyrev <php.sql.developer@gmail.com>
2018-01-02 13:55:33 +0300

    GuestBook: Refactoring.

--- index.php
+++ index.php
@@ -4,14 +4,18 @@
  * @file
  * Contains php function for start main.php page.
  */
-?>
 
-<?php define('GUESTBOOK_ROOT', getcwd()); ?>
+define('GUESTBOOK_ROOT', getcwd());
 
-<?php
 // Enable display all errors.
 error_reporting(E_ALL);
 ini_set('display_errors', '1');
-?>
 
-<?php include_once 'templates/html.php'; ?>
+require_once GUESTBOOK_ROOT . '/includes/guestbook.sql.inc';
+require_once GUESTBOOK_ROOT . '/includes/guestbook.core.inc';
+require_once GUESTBOOK_ROOT . '/includes/guestbook.theme.inc';
+
+guestbook_session_start();
+guestbook_menu_hanlder();
+
+require_once GUESTBOOK_ROOT . '/templates/html.php';
