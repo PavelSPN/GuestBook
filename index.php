0e8a5d059fbce51e6b19090372c0fdae32a481ef
PavelSPN <pavel.spn84@gmail.com>
2018-04-25 12:03:43 +0300

    add new files

--- index.php
+++ index.php
@@ -11,11 +11,11 @@ define('GUESTBOOK_ROOT', getcwd());
 error_reporting(E_ALL);
 ini_set('display_errors', '1');
 
-require_once GUESTBOOK_ROOT . '/includes/guestbook.sql.inc';
 require_once GUESTBOOK_ROOT . '/includes/guestbook.core.inc';
+require_once GUESTBOOK_ROOT . '/includes/guestbook.sql.inc';
 require_once GUESTBOOK_ROOT . '/includes/guestbook.theme.inc';
 
 guestbook_session_start();
-guestbook_menu_hanlder();
+guestbook_menu_handler();
 
 require_once GUESTBOOK_ROOT . '/templates/html.php';
